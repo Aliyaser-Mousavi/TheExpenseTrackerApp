2697218fa21d2a8ec33020ba88cb833f0268851b
Aliyaser <aliyasermousavi@gmail.com>
2026-03-08 09:42:22 +0430

    feat: work on setup project

---
+++ screens/MangeExpense.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const MangeExpenses = () => {
+  return (
+    <View>
+      <Text>MangeExpenses</Text>
+    </View>
+  );
+};
+
+export default MangeExpenses;
+
+const styles = StyleSheet.create({});
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const RecentExpense = () => {
+  return (
+    <View>
+      <Text>RecentExpense</Text>
+    </View>
+  );
+};
+
+export default RecentExpense;
+
+const styles = StyleSheet.create({});
